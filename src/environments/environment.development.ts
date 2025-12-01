268dc8e60972bfe170966a00ebc038d2ef0d2997
jao-carmo <joaoepdocarmo@gmail.com>
2025-12-01 13:44:06 -0300

    att

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://lcegnnzqejtblgrlzpqo.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZWdubnpxZWp0Ymxncmx6cHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTY2NTUsImV4cCI6MjA3MjMzMjY1NX0.Q3HDiGJzhf453jacgkS3CosEwlbEr4fOoDBKD7y4V7Y'
+  supabaseUrl: 'https://akvjkzdwsjnhfwxsheqo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrdmpremR3c2puaGZ3eHNoZXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NTcwOTUsImV4cCI6MjA3MjMzMzA5NX0.eR9cf3pUGyg21rfQ05kVcY9OJP8ScpNFrE5vHZa7fq8'
 };
